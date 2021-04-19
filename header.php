c364de2490b7f3eb9ad87dbe0d9560ae57e37378
MoLiShang <windlikely@qq.com>
2021-04-19 23:17:12 +0800

    init

---
+++ header.php
@@ -0,0 +1,44 @@
+<div class="header">
+    <div class="left">
+        <img class="logo" src="./images/logo_black.png" alt="">
+    </div>
+    <div class="right">
+        <div class="searchcontainer">
+            <input  />
+            <img src="./images/" alt="">
+        </div>
+    </div>
+</div>
+
+
+<style>
+    .header{
+        margin:0 auto;
+        margin-top:20px;
+        width: 90%;
+        height:130px;
+        display:flex;
+        justify-content:space-between;
+        align-items: center;
+    }
+    .header .left{
+        width:50%;
+    }
+    .header .left .logo{
+        width:100%;
+    }
+    .header .right .searchcontainer{
+        width:50%;
+        border-radius: 5px;
+        border:2px solid rgba(31,154,211,.99);
+    }
+
+    .header .right .searchcontainer input{
+        border:none;
+        padding:20px;
+        height:50%;
+    }
+    /* .header .right .searchcontainer input{
+
+    } */
+</style>
---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+</head>
+<body>
+<?php require 'header.php'?>
+</body>
+</html>
+
+<style>
+    body{
+        margin:0;
+        padding:0;
+    }
+</style>
